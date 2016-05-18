3b4880f2b7581f79503ce8749934fab0f2cc93bb
DZaibatsu <sirroland@yandex.com>
2016-05-18 17:17:44 +0300

    fix

--- Bootstrap.php
+++ Bootstrap.php
@@ -16,7 +16,7 @@ class Bootstrap implements BootstrapInterface
     {
         if ($app->hasModule('gii')) {
             if (!isset($app->getModule('gii')->generators['migrik'])) {
-                $app->getModule('gii')->generators['migrik'] = 'insolita\migrik\gii\Generator';
+                $app->getModule('gii')->generators['migrik'] = 'sirroland\migen\gii\Generator';
             }
         }
     }
